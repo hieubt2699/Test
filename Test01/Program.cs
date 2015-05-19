79fdcc3fb720032e0e69d8122b17a3a48b3c610c
hieubt2699 <hieubt2699@gmail.com>
2015-05-20 02:59:56 +0700

    Add
    C# project

---
+++ Test01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test01
+{
+    class Program
+    {
+        static int[] ReadArray(int n)
+        {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Phan tu a[" + i + "]: ");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            return a;
+        }
+        static void WriteArray(int[] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + "  ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr;
+            int n;
+            Console.Write("Nhap vao gia tri n: ");
+            n = int.Parse(Console.ReadLine());
+            arr = ReadArray(n);
+            WriteArray(arr, n);
+        }
+    }
+}
